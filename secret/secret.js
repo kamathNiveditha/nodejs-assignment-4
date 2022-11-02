7b30306a512c547caffb193549965ec76d8638b9
kamathNiveditha <114577012+kamathNiveditha@users.noreply.github.com>
2022-11-02 12:33:01 +0530

    Add files via upload

---
+++ secret/secret.js
@@ -0,0 +1,4 @@
+const url =
+  "mongodb+srv://Niveditha:immapro20NK@cluster0.vfcjij8.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports = url;
---
+++ services/inputValidator.js
@@ -0,0 +1,12 @@
+
+
+const emptyValueChecker = function (str) {
+    //   logger.info(`Entered inputValidator.js`);
+    if (typeof str === "string" && str.trim().length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  
+  module.exports = emptyValueChecker;
